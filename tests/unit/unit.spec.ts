589f1f7a7e7d213a136fc65ceb26f1240c43f472
Lucas Gangstad <last_talon@new.rr.com>
2022-12-15 00:07:35 -0600

    Update testing (#1)

--- tests/unit/unit.spec.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="@rbxts/testez/globals" />
-
-export = (): void => {
-	describe("Unit Test", () => {
-		it("should be true", () => {
-			expect(true).to.equal(true);
-		});
-	});
-};
