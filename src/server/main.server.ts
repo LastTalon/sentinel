62f0741797ee60a50e035ad11d55934c18b82fc7
Lucas Gangstad <lucas@lasttalon.dev>
2023-01-22 00:38:02 -0600

    Document event creation

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -3,6 +3,9 @@ import { Host } from "shared/hosts";
 import { Environment, setEnvironment } from "shared/idAttribute";
 import { getEvent } from "shared/remotes";
 
+// We only do this here at the moment to create a dummy event for replication.
+// In the future this will be created by the replication system.
 getEvent("EcsReplication");
+
 setEnvironment(Environment.Server);
 start(Host.Server);
