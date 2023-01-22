39f04d2ecde41b00f89ca0c1ee47a056cf704036
Lucas Gangstad <lucas@lasttalon.dev>
2023-01-22 00:33:10 -0600

    Update main scripts with ECS initialization

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,1 +1,8 @@
-print("Hello, world!");
+import { start } from "shared/ecs";
+import { Host } from "shared/hosts";
+import { Environment, setEnvironment } from "shared/idAttribute";
+import { getEvent } from "shared/remotes";
+
+getEvent("EcsReplication");
+setEnvironment(Environment.Server);
+start(Host.Server);
