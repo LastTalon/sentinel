995bce29e26b2eb049900760f3c376e6e7b076db
Lucas Gangstad <lucas@lasttalon.dev>
2023-01-27 12:29:47 -0600

    Add updateIdAttribute system

---
+++ src/shared/systems/shared/updateIdAttribute.ts
@@ -0,0 +1,16 @@
+import { World } from "@rbxts/matter";
+import { Model } from "shared/components";
+import { getIdAttribute } from "shared/idAttribute";
+
+/**
+ * A system that updates the ID of {@link Model | Models}.
+ *
+ * @param world - The {@link World} the system operates on
+ */
+function updateIdAttribute(world: World): void {
+	for (const [id, record] of world.queryChanged(Model)) {
+		record.new?.model?.SetAttribute(getIdAttribute(), id);
+	}
+}
+
+export = updateIdAttribute;
