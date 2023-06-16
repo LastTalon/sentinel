87f94bea2efb2f1f2cb49fd5374015a21bf8357a
Lucas Gangstad <lucas@lasttalon.dev>
2023-06-15 23:21:24 -0500

    Update home page

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -15,8 +15,8 @@ function HomepageHeader() {
 				<h1 className="hero__title">{siteConfig.title}</h1>
 				<p className="hero__subtitle">{siteConfig.tagline}</p>
 				<div className={styles.buttons}>
-					<Link className="button button--secondary button--lg" to="/docs/intro">
-						Docusaurus Tutorial - 5min ⏱️
+					<Link className="button button--secondary button--lg" to="/blog">
+						Check out our blog!
 					</Link>
 				</div>
 			</div>
