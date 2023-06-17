2dabfd95b6801d6342f7e4b764cf5490cb7e3e69
Lucas Gangstad <lucas@lasttalon.dev>
2023-06-16 23:26:55 -0500

    Add Movement component (#48)

--- src/shared/ecs/components/index.ts
+++ src/shared/ecs/components/index.ts
@@ -1,12 +1,19 @@
 import { Component, component } from "@rbxts/matter";
-import { transform } from "./defaults";
+import { gamePlacement, movement, transform } from "./defaults";
 import type {
+	GamePlacement as GamePlacementComponent,
 	Health as HealthComponent,
 	Model as ModelComponent,
+	Movement as MovementComponent,
 	Transform as TransformComponent,
 } from "./types";
 
-export type SentinelComponentType = ModelComponent | TransformComponent | HealthComponent;
+export type SentinelComponentType =
+	| ModelComponent
+	| TransformComponent
+	| HealthComponent
+	| GamePlacementComponent
+	| MovementComponent;
 export type SentinelComponent = Component<SentinelComponentType>;
 
 /**
@@ -23,6 +30,16 @@ export const Transform = component<TransformComponent>("Transform", transform);
  * The {@link HealthComponent | Health} component constructor.
  */
 export const Health = component<HealthComponent>("Health");
+
+/**
+ * The {@link MovementComponent | Movement} component constructor.
+ */
+export const Movement = component<MovementComponent>("Movement", movement);
+
+/**
+ * The {@link GamePlacementComponent | GamePlacement} component constructor.
+ */
+export const GamePlacement = component<GamePlacementComponent>("GamePlacement", gamePlacement);
 
 /**
  * This is a test component constructor.
