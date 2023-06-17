e975a1717cc5bb29fedd2cfd61f2efcc4a6955ef
Lucas Gangstad <lucas@lasttalon.dev>
2023-06-16 22:42:33 -0500

    Add Health component (#47)

--- src/shared/ecs/components/index.ts
+++ src/shared/ecs/components/index.ts
@@ -1,8 +1,12 @@
 import { Component, component } from "@rbxts/matter";
 import { transform } from "./defaults";
-import type { Model as ModelComponent, Transform as TransformComponent } from "./types";
+import type {
+	Health as HealthComponent,
+	Model as ModelComponent,
+	Transform as TransformComponent,
+} from "./types";
 
-export type SentinelComponentType = ModelComponent | TransformComponent;
+export type SentinelComponentType = ModelComponent | TransformComponent | HealthComponent;
 export type SentinelComponent = Component<SentinelComponentType>;
 
 /**
@@ -14,6 +18,11 @@ export const Model = component<ModelComponent>("Model");
  * The {@link TransformComponent | Transform} component constructor.
  */
 export const Transform = component<TransformComponent>("Transform", transform);
+
+/**
+ * The {@link HealthComponent | Health} component constructor.
+ */
+export const Health = component<HealthComponent>("Health");
 
 /**
  * This is a test component constructor.
