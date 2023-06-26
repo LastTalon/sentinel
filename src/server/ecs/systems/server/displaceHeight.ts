32323e3f723395c2991ac68a64b5d44230a98852
Lucas Gangstad <lucas@lasttalon.dev>
2023-06-26 13:21:57 -0500

    Update displaceHeight system

--- src/server/ecs/systems/server/displaceHeight.ts
+++ src/server/ecs/systems/server/displaceHeight.ts
@@ -2,11 +2,42 @@ import { World } from "@rbxts/matter";
 import { GamePlacement, HipHeight, TravelHeight } from "shared/ecs/components";
 
 function displaceHeight(world: World): void {
+	const visited: Set<number> = new Set();
+
+	for (const [id, record] of world.queryChanged(GamePlacement)) {
+		if (!record.new) continue;
+
+		let height = 0;
+		visited.add(id);
+
+		const hipHeight = world.get(id, HipHeight);
+		if (hipHeight) {
+			height += hipHeight.height;
+		}
+
+		const travelHeight = world.get(id, TravelHeight);
+		if (travelHeight) {
+			height += travelHeight.height;
+		}
+
+		if (height === record.new.offsetHeight) continue;
+
+		world.insert(
+			id,
+			record.new.patch({
+				offsetHeight: height,
+			}),
+		);
+	}
+
 	for (const [id, record] of world.queryChanged(HipHeight)) {
+		if (visited.has(id)) continue;
+
 		const placement = world.get(id, GamePlacement);
 		if (!placement) continue;
 
 		let height = 0;
+		visited.add(id);
 		if (record.new) {
 			height += record.new.height;
 		}
@@ -27,10 +58,13 @@ function displaceHeight(world: World): void {
 	}
 
 	for (const [id, record] of world.queryChanged(TravelHeight)) {
+		if (visited.has(id)) continue;
+
 		const placement = world.get(id, GamePlacement);
 		if (!placement) continue;
 
 		let height = 0;
+		visited.add(id);
 		if (record.new) {
 			height += record.new.height;
 		}
